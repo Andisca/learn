da34b86e1c8a8e53c0fb85601be168b7d1710b59
Andisca <LK755@qq.com>
2019-07-26 15:30:37 +0800

    change the class Account to the dev branch

--- learngit/src/main/java/com/itheima/pojo/Account.java
+++ learngit/src/main/java/com/itheima/pojo/Account.java
@@ -5,4 +5,20 @@ import java.io.Serializable;
 public class Account implements Serializable {
     private String owner;
     private double money;
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
 }
